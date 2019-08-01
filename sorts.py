2752f6bb2d7a055becfc650cefec4f71d384f910
user <valerasinfo@gmail.com>
2019-08-02 00:00:33 +0300

    add myInsertSort

--- sorts.py
+++ sorts.py
@@ -24,13 +24,6 @@ def bubbleSort2(alist):
 	return alist
 
 
-def get_rand_mass(size):
-	res = list()
-	for i in range(size):
-		res.append(random.randint(0, 100))
-	return res
-
-
 def choiceSort(alist):
 	for end_pointer in range(len(alist)-1, 0, -1):
 		loc_max = 0
@@ -41,13 +34,36 @@ def choiceSort(alist):
 	return alist
 
 
-def insertSort(alist):
+def myInsertSort(alist):
+	for iter_num in range(1, len(alist)):
+		flag = False
+		temp = alist[iter_num]
+		for i in range(iter_num-1, -1, -1):
+			tmp2 = alist[i]
+			if tmp2 > temp:
+				alist[i+1] = alist[i]
+				if i == 0:
+					alist[0] = temp
+				flag = True
+			if tmp2 < temp and flag:
+				alist[i+1] = temp
+				break
+			if tmp2 < temp:
+				break
 	return alist
 
 
+def get_rand_mass(size):
+	res = list()
+	for i in range(size):
+		res.append(random.randint(0, 100))
+	return res
+
+
 if __name__ == "__main__":
-	rand_mass = get_rand_mass(5)
+	rand_mass = get_rand_mass(9)
 	print(rand_mass)
 	# print('bubble sort: ', bubbleSort(rand_mass))
 	# print('bubble sort2: ', bubbleSort2(rand_mass))
-	# print('choiceSort', choiceSort(rand_mass))
+	# print('choiceSort: ', choiceSort(rand_mass))
+	print('insertSort: ', myInsertSort([54, 26, 93, 17, 77, 31, 44, 55, 20]))
